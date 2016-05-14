655a277c75e825828720161d0860d7ddf47cb97a
BohdanRybak <bogdanrybak1996@gmail.com>
2016-05-14 16:10:20 +0300

    Map zoom on location

--- app/src/main/java/com/foxunlimited/chargeshare/MainActivity.java
+++ app/src/main/java/com/foxunlimited/chargeshare/MainActivity.java
@@ -1,6 +1,12 @@
 package com.foxunlimited.chargeshare;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -14,6 +20,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     MapFragment map;
+    GoogleMapOptions options = new GoogleMapOptions();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,11 +44,31 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         tabs.setCurrentTab(0);
 
         /*Map initiqalization*/
+        options.mapType(GoogleMap.MAP_TYPE_NORMAL)
+                .compassEnabled(true)
+                .rotateGesturesEnabled(true);
         map = (MapFragment) getFragmentManager().findFragmentById(R.id.mapView);
         map.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        LocationManager lm = (LocationManager) this.getSystemService(this.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String provider = locationManager.getBestProvider(criteria, true);
+// Getting Current Location
+        Location location = locationManager.getLastKnownLocation(provider);
+        if (location != null) {
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+            LatLng latLng = new LatLng(latitude,longitude);
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,13));
+            }
     }
 }
