d30b72b53fde8746c586af8b39ca76a48ae089b1
BohdanRybak <bogdanrybak1996@gmail.com>
2016-05-15 00:18:05 +0300

    Add user parcel

--- app/src/main/java/com/foxunlimited/chargeshare/User.java
+++ app/src/main/java/com/foxunlimited/chargeshare/User.java
@@ -1,11 +1,14 @@
 package com.foxunlimited.chargeshare;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.List;
 
 /**
  * Created by bewus on 5/14/2016.
  */
-public class User {
+public class User implements Parcelable {
     public String userId;
     public String mail;
     public String pass;
@@ -20,5 +23,38 @@ public class User {
         this.mail = mail;
         this.pass = pass;
         this.nick = nick;
+    }
+
+    protected User(Parcel in) {
+        userId = in.readString();
+        mail = in.readString();
+        pass = in.readString();
+        nick = in.readString();
+    }
+
+    public static final Creator<User> CREATOR = new Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+        dest.writeString(userId);
+        dest.writeString(mail);
+        dest.writeString(pass);
+        dest.writeString(nick);
     }
 }
