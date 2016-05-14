77972212c241f699fa822393c6c967975761cfa0
bewusstlos <bewusstlos66@gmail.com>
2016-05-14 13:43:29 +0300

    base design changes

--- app/src/main/java/com/foxunlimited/chargeshare/MainActivity.java
+++ app/src/main/java/com/foxunlimited/chargeshare/MainActivity.java
@@ -2,6 +2,7 @@ package com.foxunlimited.chargeshare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TabHost tabs = (TabHost) findViewById(android.R.id.tabhost);
+        tabs.setup();
+
+        TabHost.TabSpec spec = tabs.newTabSpec("tag1");
+        spec.setContent(R.id.purposes);
+        spec.setIndicator("Purposes");
+        tabs.addTab(spec);
+
+        spec = tabs.newTabSpec("tag2");
+        spec.setContent(R.id.add_purpose);
+        spec.setIndicator("My Purposes");
+        tabs.addTab(spec);
+
+        tabs.setCurrentTab(0);
     }
 }
